93d89511b30039a8c7e2ef3a436a71cf7b64aeaa
Bishwa Bista <w0659949@selu.edu>
2021-01-14 16:30:00 -0600

    Add questions to array

--- routes/scrap.js
+++ routes/scrap.js
@@ -9,7 +9,19 @@ router.get('/', async(req, res)=>{
         const htmltags = JSON.stringify(data);
         const $ = cheerio.load(htmltags);
         const output = $('span').text();
-        res.send(output)
+        // var start_pos = output.indexOf('1.') + 1;
+        // var end_pos = output.indexOf('2.',start_pos);
+        // var text_to_get = output.substring(start_pos,end_pos);        
+        // res.send(mySubString);
+        var array=[];
+        for (i = 0 ; i < 115 ; i ++ ) {
+            var start_pos = output.indexOf((i+1) +'. ') + 1;
+            var end_pos = output.indexOf((i+ 2)+'. ',start_pos);
+            var text_to_get = output.substring(start_pos,end_pos);
+            array.push({text_to_get});
+        }
+        // console.log(array[100])
+        res.send(array)
         // res.send(data)
         // const $ = cheerio.load();
         // res.send($)
