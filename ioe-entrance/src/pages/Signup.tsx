a4ed0ccc154cbfd551617ae726b7220d7a4894c8
BISSO808 <bishwa.bista@selu.edu>
2021-08-31 20:35:32 -0500

    Changed ioe pre name

--- ioe-entrance/src/pages/Signup.tsx
+++ ioe-entrance/src/pages/Signup.tsx
@@ -1,42 +1,42 @@
-import React, { useState} from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux';
-import { IonCard, IonContent, IonCardHeader, IonText, IonInput, IonItem ,IonCardContent, IonButton} from '@ionic/react';
-import {Redirect} from "react-router-dom";
+import { IonCard, IonContent, IonCardHeader, IonText, IonInput, IonItem, IonCardContent, IonButton } from '@ionic/react';
+import { Redirect } from "react-router-dom";
 import './Signup.css';
-import {signup} from '../action/auth';
+import { signup } from '../action/auth';
 import Alert from '../components/Alert';
-import {setAlert} from '../action/alert';
+import { setAlert } from '../action/alert';
 
-const Signup=(props:any)=> {
+const Signup = (props: any) => {
     const [name, setName] = useState<string>();
     const [email, setEmail] = useState<string>();
     const [password, setPassword] = useState<string>();
     const [confirmPassword, setConfirmPassword] = useState<string>();
-    const Submit=()=>{
-        if(password=== confirmPassword){
-       signup(name, email, password);
-        } else{
+    const Submit = () => {
+        if (password === confirmPassword) {
+            signup(name, email, password);
+        } else {
             setAlert("Password doesnot match", 'danger')
         }
     }
-    if(props.isAutheticated){
+    if (props.isAutheticated) {
         return <Redirect to="/login"></Redirect>
     }
     return (
         <IonContent color="primary">
             <IonCard className="card">
                 <IonCardHeader>
-                <IonText className="title">
-                    <p>IOE ENTRANCE</p>
+                    <IonText className="title">
+                        <p>IOE ENTRANCE preparation</p>
                     </IonText>
                     <IonText className="heading">
-                    <p>SIGN UP</p>
+                        <p>SIGN UP</p>
                     </IonText>
-                    
+
                 </IonCardHeader>
-                <Alert alert={props.alert}/>
+                <Alert alert={props.alert} />
                 <IonCardContent>
-                <IonItem className="input">
+                    <IonItem className="input">
                         <IonInput value={name} placeholder="Name" onIonChange={e => setName(e.detail.value!)}></IonInput>
                     </IonItem>
                     <div className="bottom"></div>
@@ -53,21 +53,21 @@ const Signup=(props:any)=> {
                     </IonItem>
                     <div className="bottom"></div>
                     <IonButton className="button" shape="round" color="secondary" onClick={Submit}>Sign Up</IonButton>
-                    
+
                 </IonCardContent>
             </IonCard>
         </IonContent>
     )
 }
 
-const mapStateToProps=(state:any)=> ({
-alert:state.alert,
-isAutheticated:state.auth.isAuthenticated
+const mapStateToProps = (state: any) => ({
+    alert: state.alert,
+    isAutheticated: state.auth.isAuthenticated
 })
 
 Signup.propTypes = {
 
 }
 
-export default connect(mapStateToProps, {signup})(Signup)
+export default connect(mapStateToProps, { signup })(Signup)
 
